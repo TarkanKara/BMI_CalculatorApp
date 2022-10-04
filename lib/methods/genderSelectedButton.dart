8af9ad6337d43e772ea424780d23e6fb6219e3da
Tarkan Kara <unnahmet23@hotmail.com>
2022-10-04 18:21:15 +0300

    regulation GenderselectedButton

---
+++ lib/methods/genderSelectedButton.dart
@@ -0,0 +1,21 @@
+ import 'package:flutter/material.dart';
+
+GestureDetector GenderselectedButton(
+      String text, Function() onTap, Color color) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 100,
+        height: 45,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: color,
+          border: Border.all(
+            width: 1,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(40)),
+        ),
+        child: Text(text),
+      ),
+    );
+  }
